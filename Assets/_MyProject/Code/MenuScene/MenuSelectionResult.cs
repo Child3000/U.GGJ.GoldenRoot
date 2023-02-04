ac889e0e0a204ada47b0482ee19906a729af3ba4
Tan Wei Yon <tanweiyon@hotmail.com>
2023-02-05 00:28:55 +0800

    Bridged Menu scene functionality via MenuSceneManager script.

--- Assets/_MyProject/Code/MenuScene/MenuSelectionResult.cs
+++ Assets/_MyProject/Code/MenuScene/MenuSelectionResult.cs
@@ -7,19 +7,5 @@ namespace GoldenRoot.MenuScene
     {
         public int P1SelectionIndex = -1;
         public int P2SelectionIndex = -2;
-
-        public static MenuSelectionResult Singleton;
-
-        private void Awake()
-        {
-            if (Singleton != null)
-            {
-                Destroy(gameObject);
-                return;
-            }
-
-            Singleton = this;
-            DontDestroyOnLoad(this);
-        }
     }
 }
--- Assets/_MyProject/Code/UI/WorldCanvasFaceCamera.cs
+++ Assets/_MyProject/Code/UI/WorldCanvasFaceCamera.cs
@@ -5,9 +5,19 @@ namespace GoldenRoot
 {
     public class WorldCanvasFaceCamera : MonoBehaviour
     {
+        private Camera _MainCam;
+
+        public Camera MainCam
+        {
+            get
+            {
+                if (_MainCam == null) _MainCam = Camera.main;
+                return _MainCam;
+            }
+        }
         protected virtual void Update()
         {
-            var lookDir = (transform.position - Camera.main.transform.position).normalized;
+            var lookDir = (transform.position - MainCam.transform.position).normalized;
             lookDir.x = lookDir.z = 0f;
             transform.rotation = Quaternion.LookRotation(-lookDir, Vector3.forward);
         }
