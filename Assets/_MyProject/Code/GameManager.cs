b3346735bd51d56ed427f584c68fc94138d6819f
Nixon <voxell.technologies@gmail.com>
2023-02-05 12:59:39 +0800

    add bg music, ready go audio

--- Assets/_MyProject/Code/GameManager.cs
+++ Assets/_MyProject/Code/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 namespace GoldenRoot
 {
@@ -7,14 +8,24 @@ namespace GoldenRoot
 
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private Transform _CameraTransform;
         [SerializeField] private MenuSceneManager _MenuSceneManager;
-        [SerializeField] private CharacterFactory _CharacterFactory ;
 
+        [Header("Player Character")]
+        [SerializeField] private CharacterFactory _CharacterFactory ;
         [SerializeField] private Transform _P1ModelTrans;
         [SerializeField] private Transform _P2ModelTrans;
 
-        [SerializeField] private Transform _CameraTransform;
-        [SerializeField] private GameObject _CanvasGO;
+        [Header("UI")]
+        [SerializeField] private GameObject _MainMenu;
+        [SerializeField] private TextMeshProUGUI _CenterText;
+
+        [Header("Audio")]
+        [SerializeField] private AudioSource _AudioSource;
+        [SerializeField] private AudioClip _MenuClip;
+        [SerializeField] private AudioClip _GameplayClip;
+        [SerializeField] private AudioClip _ReadyGoLowPitch;
+        [SerializeField] private AudioClip _ReadyGoHighPitch;
 
         private GameState _GameState;
 
@@ -34,9 +45,23 @@ namespace GoldenRoot
             }
         }
 
+        private void Start()
+        {
+            this.PlayClip(this._MenuClip, true, 1.0f);
+        }
+
+        private void PlayClip(AudioClip clip, bool loop, float volume)
+        {
+            this._AudioSource.Stop();
+            this._AudioSource.clip = clip;
+            this._AudioSource.loop = loop;
+            this._AudioSource.volume = volume;
+            this._AudioSource.Play();
+        }
+
         public void PlayButtonPressed()
         {
-            this._CanvasGO.SetActive(false);
+            this._MainMenu.SetActive(false);
             this._MenuSceneManager.StartMenuSceneInAdditiveMode(
                 (s) =>
                 {
@@ -50,7 +75,36 @@ namespace GoldenRoot
                     Instantiate(p2Data.CharacterPrefab, this._P2ModelTrans);
 
                     this.MoveCameraToGameplay();
-                    this._GameState = GameState.Playing;
+                    this._AudioSource.Stop();
+
+                    Sequence seq = DOTween.Sequence();
+                    seq.AppendCallback(() =>
+                        {
+                            this._CenterText.text = "Ready";
+                            this._AudioSource.PlayOneShot(this._ReadyGoLowPitch);
+                        })
+                        .AppendInterval(1.0f)
+                        .AppendCallback(() =>
+                        {
+                            this._CenterText.text = "Set";
+                            this._AudioSource.PlayOneShot(this._ReadyGoLowPitch);
+                        })
+                        .AppendInterval(1.0f)
+                        .AppendCallback(() =>
+                        {
+                            this._CenterText.text = "GO!";
+                            this._AudioSource.PlayOneShot(this._ReadyGoHighPitch);
+                        })
+                        .AppendInterval(1.0f)
+                        .AppendCallback(() =>
+                        {
+                            this._CenterText.text = "";
+                            this._GameState = GameState.Playing;
+                            GameTimeManager.Singleton.StartCountdown();
+
+                            this.PlayClip(this._GameplayClip, false, 0.7f);
+                        }
+                    );
                 }
             );
         }
@@ -59,8 +113,8 @@ namespace GoldenRoot
         {
             Sequence seq = DOTween.Sequence();
             seq.Append(this._CameraTransform.DOJump(new Vector3(4f, 9f, 4f), 0.5f, 1, 3.0f))
-               .Join(this._CameraTransform.DORotate(new Vector3(90f, 0f, 0f), 3.0f, RotateMode.Fast))
-               .AppendCallback(() => this._CanvasGO.SetActive(true));
+                .Join(this._CameraTransform.DORotate(new Vector3(90f, 0f, 0f), 3.0f, RotateMode.Fast))
+                .AppendCallback(() => this._MainMenu.SetActive(true));
         }
 
         private void MoveCameraToGameplay()
